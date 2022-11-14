dff5e66c929d2e81e8bea423833b191fc6a6cada
Kailey <k.kotynski@gmail.com>
2022-11-14 13:09:49 -0800

    Added dotenv for env variables and added connection to database using mongoose

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// to access env variables
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+// to connect to the DB
+const mongoose = require('mongoose');
+// save connection string to variable and replace password with one in env variables
+const DB = process.env.DB.replace('<password>', process.env.PASSWORD);
+// use mongoose connect to connect to the DB
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch((err) => console.log(err));
+
 const app = require('./app');
 const port = 3000;
 // initial testing route
